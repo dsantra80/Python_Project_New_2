36e2b2cb81cdf5a79bb295240c0aef879d97e099
dsantra80 <60920557+dsantra80@users.noreply.github.com>
2024-05-25 19:41:44 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MODEL_PATH = os.getenv('MODEL_PATH', 'openlm-research/open_llama_3b')
+    MODEL_PATH = os.getenv('MODEL_PATH', 'openlm-research/open_llama_7b')
     MAX_TOKENS = int(os.getenv('MAX_TOKENS', 32))
     TEMPERATURE = float(os.getenv('TEMPERATURE', 0.7))
