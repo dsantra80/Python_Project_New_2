49d4f6ad311da9d5d827d1c352aef0ca9b7cff12
dsantra80 <60920557+dsantra80@users.noreply.github.com>
2024-05-25 13:30:26 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from routes import main
 from config import Config
+from routes import configure_routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.register_blueprint(main)
 
-if __name__ == '__main__':
+configure_routes(app)
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
